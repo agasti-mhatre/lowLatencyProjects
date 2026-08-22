dd62fa3e4573e5341249cbebbf2606cadc1acd7f
Agasti Mhatre <agastimhatre@Agastis-MacBook-Air.local>
2026-08-21 23:00:50 -0400

    do explanation for results

--- experiments/branch_prediction_benchmark.cpp
+++ experiments/branch_prediction_benchmark.cpp
@@ -244,53 +244,152 @@ int modX(int x)
     return res;
 }
 
-
-static void BM_ModXEq1(benchmark::State& state) {
+static void BM_ModX(benchmark::State& state) {
+    int x = static_cast<int>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(1));
+        benchmark::DoNotOptimize(modX(x));
     }
 }
 
-static void BM_ModXEq2(benchmark::State& state) {
+BENCHMARK(BM_ModX)->Arg(1);
+BENCHMARK(BM_ModX)->Arg(2);
+
+/*
+Address translations:
+
+    - x29, #0x1c -> int x
+    - x29, #0x2c -> int res
+    - x29, #0x28 -> int i
+
+
+    stp    x29, x30, [sp, #-0x30]!
+    mov    x29, sp
+    str    w0, [x29, #0x1c]          w0 = x command that was passed in. Store w0 into the address on right-hand side
+    str    wzr, [x29, #0x2c]
+    str    wzr, [x29, #0x28]
+    b      0x104669e60               jump to (ldr w1, [x29, #0x28]) command
+    ldr    w0, [x29, #0x28]          w0 = int i
+    ldr    w1, [x29, #0x1c]          w1 = int x
+    sdiv   w2, w0, w1                w2 = w0 / w1 (int i / int x)
+    ldr    w1, [x29, #0x1c]          w1 = int x
+    mul    w1, w2, w1                w1 = w2 * w1
+    sub    w0, w0, w1                w0 = w0 - w1
+    cmp    w0, #0x0                  is w0 == 0? ->
+    b.ne   0x104669e20
+    ldr    w0, [x29, #0x2c]
+    add    w0, w0, #0x1
+    str    w0, [x29, #0x2c]
+    b      0x104669e54
+    str    wzr, [x29, #0x24]
+    b      0x104669e40
+    ldr    w0, [x29, #0x2c]
+    add    w0, w0, #0x1
+    str    w0, [x29, #0x2c]
+    ldr    w0, [x29, #0x24]
+    add    w0, w0, #0x1
+    str    w0, [x29, #0x24]
+    ldr    w0, [x29, #0x1c]
+    sub    w0, w0, #0x1
+    ldr    w1, [x29, #0x24]
+    cmp    w1, w0
+    b.lt   0x104669e28
+    ldr    w0, [x29, #0x28]
+    add    w0, w0, #0x1
+    str    w0, [x29, #0x28]
+    ldr    w1, [x29, #0x28]           Load int i into register w1
+    mov    w0, #0x270f                w0 = 9999
+    cmp    w1, w0
+    b.le   0x104669df0                if i <= 9999, jump to first (ldr w0, [x29, #0x28]) command
+    ldr    w0, [x29, #0x2c]
+    ldp    x29, x30, [sp], #0x30
+    ret
+ */
+
+BENCHMARK(BM_ModX)->Arg(3);
+BENCHMARK(BM_ModX)->Arg(5);
+BENCHMARK(BM_ModX)->Arg(10);
+BENCHMARK(BM_ModX)->Arg(100);
+
+// Do [[likely]]/[[unlikely]] experiments next
+int modXLikely(int x)
+{
+    int res = 0;
+    for (int i = 0; i < 10000; ++i)
+    {
+        if ((i % x) == 0) [[likely]] { ++res; }
+        else
+        {
+            for (int i = 0; i < (x - 1); ++i) { ++res; }
+        }
+    }
+    return res;
+}
+
+int modXUnlikely(int x)
+{
+    int res = 0;
+    for (int i = 0; i < 10000; ++i)
+    {
+        if ((i % x) == 0) [[unlikely]] { ++res; }
+        else
+        {
+            for (int i = 0; i < (x - 1); ++i) { ++res; }
+        }
+    }
+    return res;
+}
+
+static void BM_ModXLikely(benchmark::State& state) {
+    int x = static_cast<int>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(2));
+        benchmark::DoNotOptimize(modXLikely(x));
     }
 }
 
-static void BM_ModXEq3(benchmark::State& state) {
+static void BM_ModXUnlikely(benchmark::State& state) {
+    int x = static_cast<int>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(3));
+        benchmark::DoNotOptimize(modXUnlikely(x));
     }
 }
 
-static void BM_ModXEq5(benchmark::State& state) {
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(5));
-    }
-}
+BENCHMARK(BM_ModXLikely)->Arg(1);
+BENCHMARK(BM_ModXLikely)->Arg(3);
+BENCHMARK(BM_ModXLikely)->Arg(5);
+BENCHMARK(BM_ModXLikely)->Arg(10);
+BENCHMARK(BM_ModXLikely)->Arg(100);
 
-static void BM_ModXEq10(benchmark::State& state) {
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(10));
-    }
-}
 
-static void BM_ModXEq100(benchmark::State& state) {
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(100));
-    }
-}
+BENCHMARK(BM_ModXUnlikely)->Arg(1);
+BENCHMARK(BM_ModXUnlikely)->Arg(3);
+BENCHMARK(BM_ModXUnlikely)->Arg(5);
+BENCHMARK(BM_ModXUnlikely)->Arg(10);
+BENCHMARK(BM_ModXUnlikely)->Arg(100);
 
-static void BM_ModXEq1000(benchmark::State& state) {
-    for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(1000));
-    }
-}
+/*
+--------------------------------------------------------------
+Benchmark                    Time             CPU   Iterations
+--------------------------------------------------------------
+BM_ModX/1                36595 ns        36591 ns        49123
+BM_ModX/2                33383 ns        33381 ns        19290
+BM_ModX/3                41815 ns        41814 ns        16639
+BM_ModX/5                49206 ns        49201 ns        14446
+BM_ModX/10               83715 ns        83706 ns         8362
+BM_ModX/100            2187797 ns      2186464 ns          390
+BM_ModXLikely/1          33894 ns        33889 ns        56034
+BM_ModXLikely/3          38147 ns        38145 ns        16647
+BM_ModXLikely/5          53549 ns        53523 ns        13888
+BM_ModXLikely/10         83445 ns        83424 ns         8420
+BM_ModXLikely/100      3054723 ns      3054718 ns          234
+BM_ModXUnlikely/1        17145 ns        17121 ns        35411
+BM_ModXUnlikely/3        39791 ns        39789 ns        16854
+BM_ModXUnlikely/5        54430 ns        54411 ns        12929
+BM_ModXUnlikely/10      113485 ns       113435 ns         6366
+BM_ModXUnlikely/100    2218717 ns      2218730 ns          293
 
-BENCHMARK(BM_ModXEq1);
-BENCHMARK(BM_ModXEq2);
-BENCHMARK(BM_ModXEq3);
-BENCHMARK(BM_ModXEq5);
-BENCHMARK(BM_ModXEq10);
-BENCHMARK(BM_ModXEq100);
-BENCHMARK(BM_ModXEq1000);
+Explanation:
+*/
+
+
+
+// TODO: Do hot path, with for(if (...)) and if(for(...))
