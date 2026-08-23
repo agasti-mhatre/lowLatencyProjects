373809c8dbebfa3994d841090f5c97c5bd4c4a88
Agasti Mhatre <agastimhatre@Agastis-MacBook-Air.local>
2026-08-23 15:07:39 -0400

    refine experiments by using optimized builds for assembly code

--- experiments/branch_prediction_benchmark.cpp
+++ experiments/branch_prediction_benchmark.cpp
@@ -230,166 +230,142 @@ static void BM_MajorityHit(benchmark::State& state)
 // that happens when a branch hit does happen. Load/add/store operations
 // increase during a branch hit. Make the work even.
 
-int modX(int x)
+
+// These next experiments all use optimized builds which require:
+// 1. target_compile_options(branch_prediction_benchmark PRIVATE -O2)
+// in the local CMakeLists.txt
+// 2. __attribute__((noinline)) attribute above each function I want
+// to put a breakpoint in
+
+static const unsigned long k = 10000000;
+
+__attribute__((noinline))
+unsigned long modX(unsigned long x)
 {
-    int res = 0;
-    for (int i = 0; i < 10000; ++i)
+    unsigned long res = 0;
+    for (unsigned long i = 0; i < k; ++i)
     {
         if ((i % x) == 0){ ++res; }
         else
         {
-            for (int i = 0; i < (x - 1); ++i) { ++res; }
+            for (unsigned long i = 0; i < (x - 1); ++i) { ++res; }
+        }
+    }
+    return res;
+}
+
+// Do [[likely]]/[[unlikely]] experiments next
+__attribute__((noinline))
+unsigned long modXLikely(unsigned long x)
+{
+    unsigned long res = 0;
+    for (unsigned long i = 0; i < k; ++i)
+    {
+        if ((i % x) == 0) [[likely]] { ++res; }
+        else
+        {
+            for (unsigned long i = 0; i < (x - 1); ++i) { ++res; }
+        }
+    }
+    return res;
+}
+
+// Including this __attribute__
+// so that this function can be debugged
+// at its breakpoints when optimized compilations
+// are enabled.
+__attribute__((noinline))
+unsigned long modXUnlikely(unsigned long x)
+{
+    unsigned long res = 0;
+    for (unsigned long i = 0; i < k; ++i)
+    {
+        if ((i % x) == 0) [[unlikely]] { ++res; }
+        else
+        {
+            for (unsigned long i = 0; i < (x - 1); ++i) { ++res; }
         }
     }
     return res;
 }
 
 static void BM_ModX(benchmark::State& state) {
-    int x = static_cast<int>(state.range(0));
+    unsigned long x = static_cast<unsigned long>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modX(x));
+        benchmark::DoNotOptimize(x);
+        auto result = modX(x);
+        benchmark::DoNotOptimize(result);
     }
-}
-
-BENCHMARK(BM_ModX)->Arg(1);
-BENCHMARK(BM_ModX)->Arg(2);
-
-/*
-Address translations:
-
-    - x29, #0x1c -> int x
-    - x29, #0x2c -> int res
-    - x29, #0x28 -> int i
-
-
-    stp    x29, x30, [sp, #-0x30]!
-    mov    x29, sp
-    str    w0, [x29, #0x1c]          w0 = x command that was passed in. Store w0 into the address on right-hand side
-    str    wzr, [x29, #0x2c]
-    str    wzr, [x29, #0x28]
-    b      0x104669e60               jump to (ldr w1, [x29, #0x28]) command
-    ldr    w0, [x29, #0x28]          w0 = int i
-    ldr    w1, [x29, #0x1c]          w1 = int x
-    sdiv   w2, w0, w1                w2 = w0 / w1 (int i / int x)
-    ldr    w1, [x29, #0x1c]          w1 = int x
-    mul    w1, w2, w1                w1 = w2 * w1
-    sub    w0, w0, w1                w0 = w0 - w1
-    cmp    w0, #0x0                  is w0 == 0? ->
-    b.ne   0x104669e20
-    ldr    w0, [x29, #0x2c]
-    add    w0, w0, #0x1
-    str    w0, [x29, #0x2c]
-    b      0x104669e54
-    str    wzr, [x29, #0x24]
-    b      0x104669e40
-    ldr    w0, [x29, #0x2c]
-    add    w0, w0, #0x1
-    str    w0, [x29, #0x2c]
-    ldr    w0, [x29, #0x24]
-    add    w0, w0, #0x1
-    str    w0, [x29, #0x24]
-    ldr    w0, [x29, #0x1c]
-    sub    w0, w0, #0x1
-    ldr    w1, [x29, #0x24]
-    cmp    w1, w0
-    b.lt   0x104669e28
-    ldr    w0, [x29, #0x28]
-    add    w0, w0, #0x1
-    str    w0, [x29, #0x28]
-    ldr    w1, [x29, #0x28]           Load int i into register w1
-    mov    w0, #0x270f                w0 = 9999
-    cmp    w1, w0
-    b.le   0x104669df0                if i <= 9999, jump to first (ldr w0, [x29, #0x28]) command
-    ldr    w0, [x29, #0x2c]
-    ldp    x29, x30, [sp], #0x30
-    ret
- */
-
-BENCHMARK(BM_ModX)->Arg(3);
-BENCHMARK(BM_ModX)->Arg(5);
-BENCHMARK(BM_ModX)->Arg(10);
-BENCHMARK(BM_ModX)->Arg(100);
-
-// Do [[likely]]/[[unlikely]] experiments next
-int modXLikely(int x)
-{
-    int res = 0;
-    for (int i = 0; i < 10000; ++i)
-    {
-        if ((i % x) == 0) [[likely]] { ++res; }
-        else
-        {
-            for (int i = 0; i < (x - 1); ++i) { ++res; }
-        }
-    }
-    return res;
-}
-
-int modXUnlikely(int x)
-{
-    int res = 0;
-    for (int i = 0; i < 10000; ++i)
-    {
-        if ((i % x) == 0) [[unlikely]] { ++res; }
-        else
-        {
-            for (int i = 0; i < (x - 1); ++i) { ++res; }
-        }
-    }
-    return res;
 }
 
 static void BM_ModXLikely(benchmark::State& state) {
-    int x = static_cast<int>(state.range(0));
+    unsigned long x = static_cast<unsigned long>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modXLikely(x));
+        benchmark::DoNotOptimize(x);
+        auto result = modX(x);
+        benchmark::DoNotOptimize(result);
     }
 }
 
 static void BM_ModXUnlikely(benchmark::State& state) {
-    int x = static_cast<int>(state.range(0));
+    unsigned long x = static_cast<unsigned long>(state.range(0));
     for (auto _ : state) {
-        benchmark::DoNotOptimize(modXUnlikely(x));
+        benchmark::DoNotOptimize(x);
+        auto result = modX(x);
+        benchmark::DoNotOptimize(result);
     }
 }
 
-BENCHMARK(BM_ModXLikely)->Arg(1);
-BENCHMARK(BM_ModXLikely)->Arg(3);
-BENCHMARK(BM_ModXLikely)->Arg(5);
-BENCHMARK(BM_ModXLikely)->Arg(10);
+BENCHMARK(BM_ModX)->Arg(100);
+BENCHMARK(BM_ModX)->Arg(117);
+BENCHMARK(BM_ModX)->Arg(500);
+BENCHMARK(BM_ModX)->Arg(10000);
+BENCHMARK(BM_ModX)->Arg(k);
+
 BENCHMARK(BM_ModXLikely)->Arg(100);
+BENCHMARK(BM_ModXLikely)->Arg(117);
+BENCHMARK(BM_ModXLikely)->Arg(500);
+BENCHMARK(BM_ModXLikely)->Arg(10000);
+BENCHMARK(BM_ModXLikely)->Arg(k);
 
-
-BENCHMARK(BM_ModXUnlikely)->Arg(1);
-BENCHMARK(BM_ModXUnlikely)->Arg(3);
-BENCHMARK(BM_ModXUnlikely)->Arg(5);
-BENCHMARK(BM_ModXUnlikely)->Arg(10);
 BENCHMARK(BM_ModXUnlikely)->Arg(100);
+BENCHMARK(BM_ModXUnlikely)->Arg(117);
+BENCHMARK(BM_ModXUnlikely)->Arg(500);
+BENCHMARK(BM_ModXUnlikely)->Arg(10000);
+BENCHMARK(BM_ModXUnlikely)->Arg(k);
 
 /*
---------------------------------------------------------------
-Benchmark                    Time             CPU   Iterations
---------------------------------------------------------------
-BM_ModX/1                36595 ns        36591 ns        49123
-BM_ModX/2                33383 ns        33381 ns        19290
-BM_ModX/3                41815 ns        41814 ns        16639
-BM_ModX/5                49206 ns        49201 ns        14446
-BM_ModX/10               83715 ns        83706 ns         8362
-BM_ModX/100            2187797 ns      2186464 ns          390
-BM_ModXLikely/1          33894 ns        33889 ns        56034
-BM_ModXLikely/3          38147 ns        38145 ns        16647
-BM_ModXLikely/5          53549 ns        53523 ns        13888
-BM_ModXLikely/10         83445 ns        83424 ns         8420
-BM_ModXLikely/100      3054723 ns      3054718 ns          234
-BM_ModXUnlikely/1        17145 ns        17121 ns        35411
-BM_ModXUnlikely/3        39791 ns        39789 ns        16854
-BM_ModXUnlikely/5        54430 ns        54411 ns        12929
-BM_ModXUnlikely/10      113485 ns       113435 ns         6366
-BM_ModXUnlikely/100    2218717 ns      2218730 ns          293
-
-Explanation:
+-------------------------------------------------------------------
+Benchmark                         Time             CPU   Iterations
+-------------------------------------------------------------------
+BM_ModX/100                 5347786 ns      5344431 ns          130
+BM_ModX/117                 5322313 ns      5312924 ns          131
+BM_ModX/500                 5336963 ns      5330664 ns          131
+BM_ModX/10000               5379748 ns      5363763 ns          131
+BM_ModX/10000000            5195748 ns      5185037 ns          135
+BM_ModXLikely/100           5336776 ns      5330623 ns          130
+BM_ModXLikely/117           5815688 ns      5651667 ns          132
+BM_ModXLikely/500           5305938 ns      5305031 ns          131
+BM_ModXLikely/10000         5318774 ns      5318331 ns          133
+BM_ModXLikely/10000000      5169148 ns      5168259 ns          135
+BM_ModXUnlikely/100         5321463 ns      5320409 ns          132
+BM_ModXUnlikely/117         5308007 ns      5307098 ns          132
+BM_ModXUnlikely/500         5314462 ns      5313386 ns          132
+BM_ModXUnlikely/10000       5306268 ns      5305458 ns          131
+BM_ModXUnlikely/10000000    5167044 ns      5165926 ns          136
 */
 
+// Analyze assembly for attribute code above
+
+/*
+modX:
+
+*/
+
+/*
+modLikelyX:
+
+*/
 
 
 // TODO: Do hot path, with for(if (...)) and if(for(...))
